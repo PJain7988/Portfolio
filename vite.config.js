07626ebdbf3fb0a1ea229544b5b364110467b296
unknown <priyajain7988@gmail.com>
2025-07-18 09:23:01 +0530

    My Portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Replace 'Portfolio' with your repo name
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/',
-});
+})
