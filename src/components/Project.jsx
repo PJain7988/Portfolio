e7d2c2090aa4ef747b3fe703457a7be8890406e3
unknown <priyajain7988@gmail.com>
2026-06-27 18:04:39 +0530

    update

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -33,7 +33,7 @@ const projects = [
   {
     imgSrc: '/images/omnisync-ai.png',
     title: 'OmniSync AI | Analytics Platform',
-    tags: ['MERN Stack', 'Machine Learning', 'NLP', 'Computer Vision'],
+    tags: ['MERN Stack', 'NLP', 'Machine Learning', 'Computer Vision'],
     projectLink: 'https://omni-sync-ai.vercel.app/',
     githubLink: 'https://github.com/PJain7988/OmniSync-AI'
   },
